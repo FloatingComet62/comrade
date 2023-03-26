fd2f01ef8b86c73d29cec5cfa317d99f518d1fce
FloatingComet62 <shubh2007rai@gmail.com>
2023-03-26 13:57:07 +0530

    Bug reporting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::env;
-
 use comrade::{exit, lexer::Parser, read_file, write_file};
+use open::that;
+use std::env;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -12,6 +12,9 @@ fn main() {
     let out_path = "out.c";
     match raw_path {
         Some(path) => {
+            if path == "report" {
+                handle_open("https://github.com/FloatingComet62/comrade/issues/new?assignees=&labels=&template=bug_report.md&title=")
+            }
             let data = read_file(&path);
             let parser = Parser::new(data);
             let (_program, c_code) = parser.parse(true, print_tokens, print_ast, print_c_code);
@@ -23,5 +26,15 @@ fn main() {
             }
         }
         None => exit("No input files passes", None),
+    }
+}
+
+fn handle_open(s: &str) {
+    match that(s) {
+        Ok(_) => exit("", Some(0)),
+        Err(e) => exit(
+            &format!("Failed to open the url\nLink: {}\nError Trace:\n{}", s, e),
+            None,
+        ),
     }
 }
